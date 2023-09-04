b8d0aedb85718627fd2d2e2234ecba456beb7b5c
Aldenor Alencar Terceiro Neto <Aldenor2302@academicos.com>
2023-09-04 20:31:29 -0300

    atividades

---
+++ # atividade.py
@@ -0,0 +1,13 @@
+# atividade
+
+
+va = int(input('digite um valor: '))
+if va > 30:
+    print('o valor é maior que 30!')
+else:
+    print('o valor é menor que 30!')
+
+
+        
+            
+            
